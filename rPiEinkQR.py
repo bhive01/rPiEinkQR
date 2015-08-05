ef730a961641b64e1b54497e78ba6567c7e886d3
Brandon Hurr <brandon.hurr@gmail.com>
2015-08-05 13:36:59 -0700

    Update rPiEinkQR.py

--- rPiEinkQR.py
+++ rPiEinkQR.py
@@ -1,25 +1,41 @@
-import os
+import os, time
 from PIL import Image
 from epyper.displayCOGProcess import Display
 from epyper.displayController import DisplayController
 
-# code to create QR code of good size for eink screen
-# qrencode -o qrcode.png -s 7 -l L -v 1 -m 1 "TestThree003"
+#initiate oldcmd
+oldcmd = ""
 
-QRname = "qrencode -o qrcode.png -s 7 -l L -v 1 -m 1 \"" + "TestThree04" + "\""
-os.system(QRname)
+while True:
+	usercmd = raw_input("Command:")
+	if usercmd == 'end':
+		break
+	else:
+		if oldcmd == usercmd:
+			pass #do nothing
+		else: 
+			#put together system command for making the QR code
+			QRname = "qrencode -o qrcode.png -s 7 -l L -v 1 -m 1 \"" + usercmd + "\""
+			# s is size of single block, l is level of code 1 = simple, m is border in blocks (relative to s)
+			# send assembled system command
+			os.system(QRname)
 
-imgName = "qrcode.png"
+			#load image for processing into correct size
+			imgName = "qrcode.png"
+			im = Image.open(imgName)
 
-im = Image.open(imgName)
+			#put together system command to pad picture with white pixels  
+			convertname = "convert " + imgName + " -background white -gravity center -extent 264x176 "+ imgName
+			#send assembled system command
+			os.system(convertname)
 
-convertname = "convert " + imgName + " -background white -gravity center -extent 264x176 "+ imgName
-os.system(convertname)
+			#reopen image after padding (need exact size for epyper)
+			im = Image.open(imgName)
 
-im = Image.open(imgName)
+			#create DisplayController instance specifying display type as an argument
+			display = DisplayController(Display.EPD_TYPE_270)
 
-#create DisplayController instance specifying display type as an argument
-display = DisplayController(Display.EPD_TYPE_270)
+			#display it!
+			display.displayImg(im)
 
-#display it!
-display.displayImg(im)
+			oldcmd = usercmd
