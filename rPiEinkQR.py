b4f9a285e5585d398a45ee404fdc5dfc38c5f816
Brandon Hurr <brandon.hurr@gmail.com>
2015-08-06 11:43:13 -0700

    Update rPiEinkQR.py

--- rPiEinkQR.py
+++ rPiEinkQR.py
@@ -19,23 +19,28 @@ while True:
 			# s is size of single block, l is level of code 1 = simple, m is border in blocks (relative to s)
 			# send assembled system command
 			os.system(QRname)
-
+			
 			#load image for processing into correct size
 			imgName = "qrcode.png"
 			im = Image.open(imgName)
-
+			
 			#put together system command to pad picture with white pixels  
 			convertname = "convert " + imgName + " -background white -gravity center -extent 264x176 "+ imgName
 			#send assembled system command
 			os.system(convertname)
-
+			
+			#add label to image
+			convertlabel = "convert " + imgName + " -rotate -90 -gravity South -pointsize 20  -font 'FreeMono-Bold' -annotate +0+2 \""+ usercmd + "\"" " -rotate 90 " + imgName
+			print(convertlabel)
+			os.system(convertlabel)
+			
 			#reopen image after padding (need exact size for epyper)
 			im = Image.open(imgName)
-
+			
 			#create DisplayController instance specifying display type as an argument
 			display = DisplayController(Display.EPD_TYPE_270)
-
+			
 			#display it!
 			display.displayImg(im)
-
+			
 			oldcmd = usercmd
