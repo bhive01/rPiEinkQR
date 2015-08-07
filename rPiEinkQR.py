84cc1a539cdd6096f9fe5298380076c480e9372a
Brandon Hurr <brandon.hurr@gmail.com>
2015-08-07 10:15:14 -0700

    Update rPiEinkQR.py
    Cleaned up comments to explain better what's happening at each stage.

--- rPiEinkQR.py
+++ rPiEinkQR.py
@@ -7,14 +7,18 @@ from epyper.displayController import DisplayController
 oldcmd = ""
 
 while True:
+	# takes input from the command line. Keyboard or scanner emulating a keyboard. newline/return to send.
 	usercmd = raw_input("Command:")
+	
+	# test for 'end' so we can stop the script when done
 	if usercmd == 'end':
 		break
 	else:
+		# are oldcmd and usercmd the same? If so, nothing new has been input, go around the loop again
 		if oldcmd == usercmd:
-			pass #do nothing
+			pass #do nothing, need pass, can't have empty if in python
 		else: 
-			#put together system command for making the QR code
+			#put together system command for making the QR code from usercmd
 			QRname = "qrencode -o qrcode.png -s 7 -l L -v 1 -m 1 \"" + usercmd + "\""
 			# s is size of single block, l is level of code 1 = simple, m is border in blocks (relative to s)
 			# send assembled system command
@@ -24,12 +28,12 @@ while True:
 			imgName = "qrcode.png"
 			im = Image.open(imgName)
 			
-			#put together system command to pad picture with white pixels  
+			#put together system command to pad picture with white pixels and put it on the right end of the image 
 			convertname = "convert " + imgName + " -background white -gravity east -extent 264x176 "+ imgName
 			#send assembled system command
 			os.system(convertname)
 			
-			#add label to image
+			#add text label of usercmd to image for human readabilty
 			convertlabel = "convert " + imgName + " -rotate -90 -gravity South -pointsize 20 -font 'FreeMono-Bold' -annotate +0+2 \""+ usercmd + "\"" " -rotate 90 " + imgName
 			print(convertlabel)
 			os.system(convertlabel)
@@ -43,4 +47,5 @@ while True:
 			#display it!
 			display.displayImg(im)
 			
+			# set oldcmd to the usercmd so it's ready for next loop test
 			oldcmd = usercmd
